9bc471197a58931a366128da80b93494f2b276d9
Thimo265 <met-010-16@must.ac.mw>
2023-03-21 13:52:37 +0000

    another fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,19 @@
 /**
  * main - contains code that prints putchar
  *
+ *Description: prints _putchar with putchar prototype
+
  * Return: always 0
  */
 
 int main(void)
 {
-	printf("putchar");
+	char str[] = "_putchar";
+	int ch;
+	
+	for (ch = 0; ch < 1; ch ++)
+		_putchar(str[ch]);
+	_putchar('\n');
 
 	return (0);
 }
