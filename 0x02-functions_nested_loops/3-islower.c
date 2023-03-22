b4f0df56ed4e638b9039f45e27f2aff41110ff0b
Thimo265 <met-010-16@must.ac.mw>
2023-03-22 21:13:54 +0000

    second

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - chunk of code to check case of letters
+ *
+ * @c: is char
+ *
+ * Return: return 1 is it is equal and 0 othwerwise
+ */
+int _islower(int c)
+{
+	int j;
+
+	for (j = 'a'; j = 'z'; j++)
+	{
+		if (j == c)
+		{
+			return (1);
+		}
+		else
+			return (0);
+	}
+}
