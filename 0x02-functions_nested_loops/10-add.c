bafcb2e1fdcbde865db475cb8b5a2aa7296be7fd
Thimo265 <125440712+Thimo265@users.noreply.github.com>
2023-03-22 15:28:07 -0700

    Create 10-add.c
    last exercise

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add -  adds two integers
+ * @x: interger 1
+ * @y: integer 2
+ *
+ * Return: addition result
+ */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
