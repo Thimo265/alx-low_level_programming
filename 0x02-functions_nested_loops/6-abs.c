e5c6eef9ce5eb001fb614e2e5dbe0e5889dbc4d3
Thimo265 <met-010-16@must.ac.mw>
2023-03-22 22:15:30 +0000

    ihhhh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "maiin.h"
+
+/**
+ * _abs - all you need
+ *
+ * @i: return value
+ *
+ * Return: absolute vale
+ */
+
+int _abs(int i)
+{
+	int j;
+
+	if (i >= 0)
+	{
+		j = i;
+	}
+	else
+	{
+		j = -i;
+	}
+	return (j);
+}
