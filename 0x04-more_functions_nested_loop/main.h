e52d6d11807cd6484e6b6ae6126ada50f9705b68
Thimo265 <125440712+Thimo265@users.noreply.github.com>
2023-03-24 15:40:59 -0700

    Update main.h

--- 0x04-more_functions_nested_loop/main.h
+++ 0x04-more_functions_nested_loop/main.h
@@ -1,7 +1,12 @@
-int _isupper(int c);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -11,3 +16,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
+#endif /* MAIN_H */
